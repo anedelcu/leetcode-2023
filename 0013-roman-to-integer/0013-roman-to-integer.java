090c6ab5683bc632055c871efbd8fce1a6bd24f8
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-02-16 09:25:07 -0600

    Time: 5 ms (65.55%), Space: 43.1 MB (28.59%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> symbolValues = new HashMap<>();
+        symbolValues.put('I', 1);
+        symbolValues.put('V', 5);
+        symbolValues.put('X', 10);
+        symbolValues.put('L', 50);
+        symbolValues.put('C', 100);
+        symbolValues.put('D', 500);
+        symbolValues.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = symbolValues.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+}
