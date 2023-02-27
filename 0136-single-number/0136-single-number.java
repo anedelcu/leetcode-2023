3f5a30b6d64cb0889e7bfe84c4a3402619040007
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-02-27 12:18:24 -0600

    Time: 1 ms (100.00%), Space: 43.1 MB (24.06%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -3,7 +3,6 @@ class Solution {
         int n = 0;
         for(int i = 0; i < nums.length; i++) {
             n = n ^ nums[i];
-            System.out.println(n + " and " + nums[i] + " = " + n);
         }
         return n;
     }
