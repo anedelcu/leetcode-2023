833e2bf3d80a78e2223458912b76063985773746
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-01 17:28:30 -0600

    Time: 1 ms (99.93%), Space: 45.9 MB (65.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,18 @@
 class Solution {
+
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        // find the element with frequency greater than n/2
-        for (int num : nums) {
-            if (map.get(num) > nums.length/2) {
-                return num;
+        int count = 0;
+        int majority = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                majority = nums[i];
+            }
+            if (nums[i] == majority) {
+                count++;
+            } else {
+                count--;
             }
         }
-        
-        // this should never be reached since majority element is guaranteed to exist
-        return -1;
+        return majority;
     }
 }
