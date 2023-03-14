0a078dc914fd967337c0eb6c47d79c14b7f58f73
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-14 14:55:11 -0500

    Time: 6 ms (71.12%), Space: 42.9 MB (26.17%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,16 +1,24 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>(wordDict);
-        boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-        for(int i = 1; i <= s.length(); i++) {
-            for(int j = 0; j < i; j++) {
-                if(dp[j] && set.contains(s.substring(j, i))) {
-                    dp[i] = true;
-                    break;
-                }
+        Boolean[] memo = new Boolean[s.length()]; // Use a memo array to store intermediate results
+        return canBreak(s, 0, set, memo);
+    }
+    
+    private boolean canBreak(String s, int start, Set<String> set, Boolean[] memo) {
+        if (start == s.length()) {
+            return true; // Base case: reached the end of s
+        }
+        if (memo[start] != null) {
+            return memo[start]; // If the result is already computed, return it
+        }
+        for (int end = start + 1; end <= s.length(); end++) {
+            if (set.contains(s.substring(start, end)) && canBreak(s, end, set, memo)) {
+                memo[start] = true;
+                return true;
             }
         }
-        return dp[s.length()];
+        memo[start] = false;
+        return false;
     }
 }
