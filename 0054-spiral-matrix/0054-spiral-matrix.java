80e609feeb3785f99255dd5bb3fc281fadd55642
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-15 14:27:10 -0500

    Time: 0 ms (100.00%), Space: 41 MB (14.71%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,34 +1,30 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> result = new ArrayList<>();
-        if(matrix.length == 0) {
-            return result;
-        }
-        
-        int direction = 0;
+        List<int[]> list = new ArrayList<>();
+        int direction  = 1; // 1 - right. 2 - down, 3 -left, 4 - up
         int left = 0;
         int right = matrix[0].length - 1;
-        int top = 0;
+        int top  = 0;
         int bottom = matrix.length - 1;
-        
         while(left <= right && top <= bottom) {
-            if(direction == 0) {
-                for(int i = left; i<= right; i++) {
+            if(direction == 1) {
+                for(int i = left; i <= right; i++) {
                     result.add(matrix[top][i]);
                 }
                 top++;
             }
-            else if(direction == 1) {
+            else if(direction == 2) {
                 for(int i = top; i <= bottom; i++) {
                     result.add(matrix[i][right]);
                 }
-                right--;
+                right --;
             }
-            else if(direction == 2) {
+            else if(direction == 3) {
                 for(int i = right; i >= left; i--) {
                     result.add(matrix[bottom][i]);
                 }
-                bottom--;
+                    bottom--;
             }
             else {
                 for(int i = bottom; i >= top; i--) {
@@ -36,9 +32,8 @@ class Solution {
                 }
                 left++;
             }
-            direction = (direction + 1) % 4;
+            direction = ( 1 + direction) % 4;
         }
         return result;
-        
     }
 }
