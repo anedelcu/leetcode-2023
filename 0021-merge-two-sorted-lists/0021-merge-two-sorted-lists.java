07c6c96cba5630d1de3adcc2cf451904d32e74d8
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-15 11:03:50 -0500

    Time: 0 ms (100.00%), Space: 42.4 MB (21.49%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -17,24 +17,31 @@ class Solution {
         if (list2 == null) {
             return list1;
         }
-        ListNode result = new ListNode(-101);
-        ListNode res = result;
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                res.next = list1;
+        ListNode result = null;
+        if(list1.val <= list2.val) {
+                result = list1;
                 list1 = list1.next;
             } else {
-                res.next = list2;
+                result = list2;
                 list2 = list2.next;
             }
-            res = res.next;
+        ListNode curr = result;
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
         }
-        if (list1 != null) {
-            res.next = list1;
+        if(list1 != null) {
+            curr.next = list1;
         }
-        if (list2 != null) {
-            res.next = list2;
+        if(list2 != null) {
+            curr.next = list2;
         }
-        return result.next;
+        return result;
     }
 }
