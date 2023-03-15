d115c421c22532e9c0317012962080aa220806ad
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-15 10:32:48 -0500

    Time: 1 ms (100.00%), Space: 52 MB (28.83%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,7 +4,7 @@ class Solution {
         int max = nums[0];
         for(int i = 1; i < nums.length; i++) {
             sum = Math.max(nums[i], sum + nums[i]);
-            max = Math.max(sum, max);
+            max = Math.max(max, sum);
         }
         return max;
         
