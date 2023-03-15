5d34bdb54bf248fd50aec37fee7c1c07331e82a9
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-15 12:27:43 -0500

    Time: 7 ms (80.65%), Space: 42.4 MB (70.69%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int calculate(String s) {
+        int result  = 0;
+        int num = 0;
+        int sign = 1;
+        Stack<Integer> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(c)) {
+                num = num * 10 + (c - '0');
+            }
+            else if(c == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if(c == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if(c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if(c == ')') {
+                
+                result += sign * num;
+                num = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+        if(num != 0) {
+            result += sign * num;
+        }
+        
+        
+        return result;
+    }
+}
