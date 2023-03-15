57a90adac15d05b2de0bcc295567d54b99ea0bcf
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-15 14:05:01 -0500

    Time: 4 ms (100.00%), Space: 42.3 MB (35.14%) - LeetHub

--- 0284-peeking-iterator/0284-peeking-iterator.java
+++ 0284-peeking-iterator/0284-peeking-iterator.java
@@ -2,42 +2,40 @@
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
 
 class PeekingIterator implements Iterator<Integer> {
-    
     private Iterator<Integer> iterator;
-    private Integer el;
-	public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
+    private Integer element;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        // initialize any member here.
         this.iterator = iterator;
-        this.el  = null;
-       
-	    
-	}
-	
+        this.element = null;
+    }
+
     // Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
-        if(this.el == null && iterator.hasNext()) {
-            el = iterator.next();
+    public Integer peek() {
+        if(element == null && iterator.hasNext()) {
+            element = iterator.next();
         }
-        return el;
-	}
-	
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
-        if(el != null) {
-            Integer newEl = el;
-            el = null;
-            return newEl;
+        return element;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        if(element != null) {
+            Integer newElement = element;
+            element = null;
+            return newElement;
         }
-	    return iterator.next();
-	}
-	
-	@Override
-	public boolean hasNext() {
-	    if(el != null || iterator.hasNext()) {
+        return iterator.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(element != null || iterator.hasNext()) {
             return true;
         }
         return false;
-	}
+    }
 }
