b7c69093e5bb2a5920a5639168ecac2d95cc629f
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-22 18:49:26 -0500

    Time: 1 ms (100.00%), Space: 41.3 MB (40.75%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,16 +1,18 @@
 class Solution {
+
     public int longestPalindrome(String s) {
         int[] count = new int[128];
         int result = 0;
-        for(char c : s.toCharArray()) {
+        for (char c : s.toCharArray()) {
             count[c]++;
         }
-        for(int num : count) {
-            result += num / 2 * 2;
-            if(result % 2 == 0 && num % 2 == 1) {
-                result++;
+        for (int num : count) {
+            if (num > 1 && num % 2 == 0) {
+                result += num;
+            } else if (num > 1 && num % 2 == 1) {
+                result += num - 1;
             }
         }
-        return result;
+        return s.length() == result ? result : result + 1;
     }
 }
