775b6e25d1f21bd4ebbd5a6bd48c27ba79c2accf
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-22 16:39:18 -0500

    Time: 3 ms (72.71%), Space: 42.9 MB (54.90%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,18 +1,17 @@
 class Solution {
+
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] letters = new int[26];
-        for(char c : magazine.toCharArray()) {
-            letters[c - 'a']++;
+        int[] count = new int[26]; // to keep track of frequency of each letter in magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            count[magazine.charAt(i) - 'a']++; // increment frequency of the corresponding letter
         }
-        for(char c : ransomNote.toCharArray()) {
-            letters[c - 'a']--;
-        }
-        for(int n: letters) {
-            if( n < 0) {
-                return false;
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int index = ransomNote.charAt(i) - 'a'; // get the index of the current letter in ransomNote
+            if (count[index] <= 0) {
+                return false; // if frequency of this letter is already zero, we can't construct ransomNote
             }
+            count[index]--; // decrement frequency of the corresponding letter in magazine
         }
         return true;
-        
     }
 }
