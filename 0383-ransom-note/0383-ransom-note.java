4243efe3b72a9312f731e47e049611ddc3cf5327
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-22 16:33:55 -0500

    Time: 24 ms (17.84%), Space: 43.4 MB (19.39%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote.length() > magazine.length()) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : magazine.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for(char c : ransomNote.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) - 1);
+        }
+        for(int val : map.values()) {
+            System.out.println(val);
+            if(val < 0) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
