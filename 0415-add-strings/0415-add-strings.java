7c8a5f22f0ac252f66e274b693f7efab819e1073
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-23 14:49:18 -0500

    Time: 2 ms (91.31%), Space: 42.9 MB (28.43%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,37 +1,20 @@
 class Solution {
+
     public String addStrings(String num1, String num2) {
-        int[] result = new int[Math.max(num1.length(), num2.length()) + 1];
-        int index = 0;
-        for(int i = num1.length() - 1; i >= 0; i--) {
-            char c = num1.charAt(i);
-            result[index] += Character.getNumericValue(c);
-            index++;
-        }
-        index = 0;
-        for(int i = num2.length() - 1; i >= 0; i--) {
-            char c = num2.charAt(i);
-            result[index] += Character.getNumericValue(c);
-            index++;
-        }
-        int carry =0;
-        for(int i = 0; i < result.length; i++) {
-            if(carry + result[i] > 9) {
-                result[i] = carry + result[i] - 10;
-                carry = 1;
-            }
-            else {
-                result[i] = carry + result[i];
-                carry = 0;
-            }
-        }
-        
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
         StringBuilder sb = new StringBuilder();
-        for(int i = result.length - 1; i >= 0; i--) {
-            if(i == result.length - 1 && result[i] == 0) {
-                continue;
-            }
-            sb.append(result[i]);
+        while(j>= 0 || i >= 0 || carry > 0) {
+            int n1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int n2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+            int sum = 0;
+            sum = n1 + n2 + carry;
+            sb.append(sum % 10);
+            carry = sum / 10;
+            i--;
+            j--;
         }
-        return sb.toString();
+        return sb.reverse().toString();
     }
 }
