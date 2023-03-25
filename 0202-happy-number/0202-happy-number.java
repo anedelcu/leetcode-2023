c5bf029f32f337530ab55016a8c6988e8005c3fe
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-25 17:37:44 -0500

    Time: 1 ms (90.07%), Space: 40.3 MB (16.81%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -2,9 +2,7 @@ class Solution {
     public boolean isHappy(int n) {
 
         Set<Integer> set = new HashSet<>();
-        if(n == 1) {
-            return true;
-        }
+        
         while(n != 1 && !set.contains(n)) {
             set.add(n);
             int sum = 0;
