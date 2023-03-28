b1046773ca8dfbbe137a5321d2967e75fe1ce6c2
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-03-28 18:41:01 -0500

    Time: 1 ms (79.60%), Space: 40.1 MB (53.07%) - LeetHub

---
+++ 0504-base-7/0504-base-7.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String convertToBase7(int num) {
+        if(num == 0) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder("");
+        boolean sign = false;
+        if(num < 0) {
+            sign = true;
+        }
+        while(num != 0) {
+            sb.append(Math.abs(num % 7));
+            num = num / 7;
+        }
+        if(sign) {
+            sb.append("-");
+        }
+        return sb.reverse().toString();
+        
+    }
+}
