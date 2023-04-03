00b27140fa0ae3efeebb18f7cc296137718d16a2
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-04-03 16:25:17 -0500

    Time: 1 ms (100.00%), Space: 42.8 MB (92.95%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,20 +1,10 @@
 class Solution {
-
     public int pivotIndex(int[] nums) {
-        int sum1 = 0;
-        int sum2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum2 += nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            
-            System.out.println("Sum1 = " + sum1);
-            System.out.println("Sum2 = " + sum2);
-            if(sum1 == sum2 - nums[i]) {
-                return i;
-            }
-            sum2 -= nums[i];
-            sum1 +=nums[i];
+        int sum = 0, leftsum = 0;
+        for (int x: nums) sum += x;
+        for (int i = 0; i < nums.length; ++i) {
+            if (leftsum == sum - leftsum - nums[i]) return i;
+            leftsum += nums[i];
         }
         return -1;
     }
