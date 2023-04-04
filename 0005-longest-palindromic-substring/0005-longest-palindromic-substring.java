a7c8d337d0a6fba997af4b0b603d0f4081b8671b
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-04-04 14:05:55 -0500

    Time: 10 ms (96.78%), Space: 42.6 MB (62.27%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,35 +1,42 @@
 class Solution {
 
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] table = new boolean[n][n];
-        int maxLength = 1;
-        int start = 0;
-        // initialize table for substrings of length 1
-        for(int i = 0; i < n; i++) {
-            table[i][i] = true;
-        }
-        // initialize table for substrings of length 2
-        for(int i = 0; i < n - 1; i++) {
-            if(s.charAt(i) == s.charAt(i + 1)) {
-                table[i][i + 1] = true;
-                maxLength = 2;
-                start = i;
+        if (s == null || s.length() < 1) return "";
+        String processed = preprocess(s);
+        int n = processed.length();
+        int[] lengths = new int[n];
+        int center = 0, right = 0;
+        for (int i = 1; i < n - 1; i++) {
+            int mirror = 2 * center - i;
+            if (right > i) {
+                lengths[i] = Math.min(right - i, lengths[mirror]);
+            }
+            while (processed.charAt(i + lengths[i] + 1) == processed.charAt(i - lengths[i] - 1)) {
+                lengths[i]++;
+            }
+            if (i + lengths[i] > right) {
+                center = i;
+                right = i + lengths[i];
             }
         }
-        // fill table for substrings of length 3 and above
-        for(int k = 3; k <= n; k++) {
-            for(int i = 0; i < n - k + 1; i++) {
-                int j = i + k - 1;
-                if(s.charAt(i) == s.charAt(j) && table[i + 1][j - 1]) {
-                    table[i][j] = true;
-                    if(k > maxLength) {
-                        maxLength = k;
-                        start = i;
-                    }
-                }
+        int maxLen = 0, centerIdx = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (lengths[i] > maxLen) {
+                maxLen = lengths[i];
+                centerIdx = i;
             }
         }
-        return  s.substring(start, start + maxLength);
+        return s.substring((centerIdx - maxLen) / 2, (centerIdx + maxLen) / 2);
+    }
+
+    private String preprocess(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('^');
+        for (int i = 0; i < s.length(); i++) {
+            sb.append('#');
+            sb.append(s.charAt(i));
+        }
+        sb.append("#$");
+        return sb.toString();
     }
 }
