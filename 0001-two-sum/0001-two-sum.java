0868700aac390f34bec8c7f6097d0be29e585e9d
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-04-04 09:08:42 -0500

    Time: 2 ms (81.97%), Space: 42.6 MB (67.59%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,17 @@
 class Solution {
 
     public int[] twoSum(int[] nums, int target) {
+        
+        // build a map val -index
+        // iterate nums array
+        // check if map.containsKey target - nums[i] and return the result if YES
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             if(map.containsKey(target - nums[i])) {
-                return new int[]{map.get(target - nums[i]), i};
+                return new int[]{i, map.get(target - nums[i])};
             }
             map.put(nums[i], i);
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
