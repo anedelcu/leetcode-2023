5720e449844c18df835fbc06118b26b4084823e4
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-04-10 10:44:07 -0500

    Time: 2 ms (79.07%), Space: 40.7 MB (33.15%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,43 +1,38 @@
 class Solution {
 
     public static boolean isValid(String s) {
-        if(s == null || s.isEmpty()) {
+        if (s == null || s.isEmpty()) {
             return false;
         }
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray()) {
-            if(stack.isEmpty()) {
-                if(c == ']' || c == ')' || c == '}') {
+        for (char c : s.toCharArray()) {
+            if (stack.isEmpty()) {
+                if (c == ']' || c == ')' || c == '}') {
                     return false;
-                }
-                else {
+                } else {
                     stack.push(c);
                 }
-            }
-            else {
-                if(c == '(' || c == '[' || c == '{') {
+            } else {
+                if (c == '(' || c == '[' || c == '{') {
                     stack.push(c);
-                }
-                else if(c == ')' ) {
+                } else if (c == ')') {
                     char lastChar = stack.pop();
-                    if(lastChar != '(') {
+                    if (lastChar != '(') {
                         return false;
                     }
-                }
-                else if(c == ']' ) {
+                } else if (c == ']') {
                     char lastChar = stack.pop();
-                    if(lastChar != '[') {
+                    if (lastChar != '[') {
                         return false;
                     }
-                }else if(c == '}' ) {
+                } else if (c == '}') {
                     char lastChar = stack.pop();
-                    if(lastChar != '{') {
+                    if (lastChar != '{') {
                         return false;
                     }
                 }
             }
         }
         return stack.isEmpty();
-        
     }
 }
