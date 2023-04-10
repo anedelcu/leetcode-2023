2c8439a7fdc95c21b1e454157c92ee867e0befe2
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-04-10 09:49:12 -0500

    Time: 2311 ms (5.03%), Space: 40.9 MB (5.03%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.java
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -13,11 +13,8 @@ class Solution {
         if ((dividend < 0 && divisor < 0)) {
             sign = 1;
         }
-        System.out.println("sign: " + sign);
         long absDividend = Math.abs((long)dividend);
-        System.out.println("dividend: " + absDividend);
         long absDivisor = Math.abs((long)divisor);
-        System.out.println("divisor: " + absDivisor);
         int count = 0;
         long result = 0;
         if(absDivisor == 1) {
