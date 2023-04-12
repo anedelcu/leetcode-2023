d458f0b66307b1652c5eaad0cee5350b67f04968
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-04-11 20:08:08 -0500

    Time: 25 ms (14.36%), Space: 43.5 MB (13.56%) - LeetHub

--- 0748-shortest-completing-word/0748-shortest-completing-word.java
+++ 0748-shortest-completing-word/0748-shortest-completing-word.java
@@ -1,41 +1,36 @@
+import java.util.*;
+
 class Solution {
+
     public String shortestCompletingWord(String licensePlate, String[] words) {
-        //Map<Character, Integer> mapLp = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        for(char c : licensePlate.toCharArray()) {
-            if(Character.isLetter(c)) {
-                //mapLp.put(c, map.getOrDefault(c, 0) + 1);
-                sb.append(c);
+        // Preprocess licensePlate string
+        licensePlate = licensePlate.toLowerCase();
+        licensePlate = licensePlate.replaceAll("[^a-z]", "");
+
+        // Initialize shortest completing word to null and maximum length to infinity
+        String shortestWord = null;
+        int maxLength = Integer.MAX_VALUE;
+
+        // Iterate through each word in words array
+        for (String word : words) {
+            // Check if word contains all characters in licensePlate
+            String tempLicensePlate = licensePlate;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+                if (tempLicensePlate.indexOf(c) != -1) {
+                    tempLicensePlate = tempLicensePlate.replaceFirst(String.valueOf(c), "");
+                    if (tempLicensePlate.isEmpty()) {
+                        // All characters in licensePlate found in word
+                        if (word.length() < maxLength) {
+                            shortestWord = word;
+                            maxLength = word.length();
+                        }
+                        break;
+                    }
+                }
             }
         }
-        String lp = sb.toString().toLowerCase();
-        StringBuilder res = new StringBuilder();
-        int minLength = 15;
-        for(String word : words) {
-            if(isCompleting(lp, word) && word.length() < minLength) {
-                res = new StringBuilder(word);
-                minLength = word.length();
-            }
-        }
-        return res.toString();
-    }
-    
-    public boolean isCompleting(String lp, String w) {
-        if(lp.length() > w.length()) {
-            return false;
-        }
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c : w.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        for(char c : lp.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) - 1);
-        }
-        for(int n : map.values()) {
-            if(n < 0) {
-                return false;
-            }
-        }
-        return true;
+
+        return shortestWord;
     }
 }
