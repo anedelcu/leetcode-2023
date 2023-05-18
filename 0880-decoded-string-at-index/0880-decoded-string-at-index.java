ac5a8548f5add98ea80a97ad556de60f56c34c28
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-05-18 16:25:43 -0500

    Time: 0 ms (100.00%), Space: 41 MB (6.02%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public String decodeAtIndex(String s, int k) {
+        long size = 0;
+        int n = s.length();
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                size = size * (ch - '0');
+            } else {
+                size++;
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            k %= size;
+            if ((k == 0 || k == size) && Character.isLetter(ch)) {
+                return Character.toString(ch);
+            }
+            if (Character.isDigit(ch)) {
+                size = size / (ch - '0');
+            } else {
+                size--;
+            }
+        }
+        return null;
+    }
+}
