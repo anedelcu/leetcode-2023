0b97db69263b881ee2beda35327ffb90a1ecb9ba
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-06-07 09:59:59 -0500

    Time: 190 ms (16.32%), Space: 44.6 MB (15.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,42 +1,40 @@
 class Solution {
+    int[] tDict = new int[256];
+    int[] sDict = new int[256];
+    char[] sa;
 
     public String minWindow(String s, String t) {
-        if (s.length() == 0 || t.length() == 0) {
-            return "";
+        sa = s.toCharArray();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            tDict[c - 'A']++;
         }
-        Map<Character, Integer> tMap = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
-        }
+        int minL = Integer.MAX_VALUE;
+        String result = "";
         int start = 0;
-        int end = 0;
-        int minLen = Integer.MAX_VALUE;
-        int count = t.length();
-        int minStart = 0;
-        Map<Character, Integer> sMap = new HashMap<>();
-        while (end < s.length()) {
-            char endChar = s.charAt(end);
-            sMap.put(endChar, sMap.getOrDefault(endChar, 0) + 1);
-            if (tMap.containsKey(endChar) && sMap.get(endChar) <= tMap.get(endChar)) {
-                count--;
+        for (int end = 0; end < s.length(); end++) {
+            sDict[sa[end] - 'A']++;
+            if (!checkValid()) {
+                continue;
             }
+            while (checkValid()) {
+                if (end - start + 1 < minL) {
+                    minL = end - start + 1;
+                    result = s.substring(start, end + 1);
+                }
+                sDict[sa[start] - 'A']--;
+                start++;
+            }
+        }
+        return result;
+    }
 
-            while (count == 0) {
-                if (end - start < minLen) {
-                    minLen = end - start;
-                    minStart = start;
-                }
-                char startChar = s.charAt(start++);
-                if (tMap.containsKey(startChar) && sMap.get(startChar) <= tMap.get(startChar)) {
-                    count++;
-                }
-                sMap.put(startChar, sMap.getOrDefault(startChar, 0) - 1);
+    private boolean checkValid() {
+        for (int i = 0; i < tDict.length; i++) {
+            if (tDict[i] > 0 && sDict[i] < tDict[i]) {
+                return false;
             }
-            end++;
         }
-        if (minLen == Integer.MAX_VALUE) {
-            return "";
-        }
-        return s.substring(minStart, minStart + minLen + 1);
+        return true;
     }
 }
