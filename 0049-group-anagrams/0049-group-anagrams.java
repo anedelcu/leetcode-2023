3ef106e109920dd541c1602905ccd19829205e29
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-08-23 12:53:40 -0500

    Time: 6 ms (88.12%), Space: 46.9 MB (61.22%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,17 +2,14 @@ class Solution {
 
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, ArrayList<String>> map = new HashMap<>();
-        for (String s : strs) {
+        for(String s : strs) {
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
             String sortedS = new String(chars);
-
-            if (!map.containsKey(sortedS)) {
-                map.put(sortedS, new ArrayList<>());
-            }
-            map.get(sortedS).add(s);
+            ArrayList<String> anagrams = map.getOrDefault(sortedS, new ArrayList<String>());
+            anagrams.add(s);
+            map.put(sortedS, anagrams);
         }
-
         return new ArrayList<>(map.values());
     }
 }
