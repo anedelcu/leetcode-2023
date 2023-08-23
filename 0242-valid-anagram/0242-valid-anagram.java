0714cf0d62097212033a9664381551d7784512c0
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-08-23 12:22:35 -0500

    Time: 1 ms (99.97%), Space: 43.1 MB (69.35%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,6 +1,9 @@
 class Solution {
 
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
         int[] arr = new int[26];
         for (char c : s.toCharArray()) {
             arr[c - 'a']++;
