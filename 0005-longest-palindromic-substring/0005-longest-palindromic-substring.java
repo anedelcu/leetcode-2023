9a1cfefd955aa0a1ea0c1bd6c181e6338ff8b07b
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-09-05 20:22:07 -0500

    Time: 18 ms (73.26%), Space: 41 MB (92.10%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,34 +1,33 @@
 class Solution {
+
     public String longestPalindrome(String s) {
-        String pal = "";
         int maxLen = 0;
         int n = s.length();
         int start = 0;
         int end = 0;
-        
-        
-        for(int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++) {
             // odd length
             int left = i;
             int right = i;
-            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > maxLen) {
-                    maxLen = right -left + 1;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > maxLen) {
+                    maxLen = right - left + 1;
                     start = left;
                     end = right;
-                } 
+                }
                 left--;
                 right++;
             }
             // even length
             left = i;
             right = i + 1;
-            while(left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > maxLen) {
-                    maxLen = right -left + 1;
+            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+                if (right - left + 1 > maxLen) {
+                    maxLen = right - left + 1;
                     start = left;
                     end = right;
-                } 
+                }
                 left--;
                 right++;
             }
