28886607beedd2950b368b88af4eaae44c0a1e50
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-09-06 11:35:09 -0500

    Time: 1 ms (86.79%), Space: 39.1 MB (89.54%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,22 +1,21 @@
 class Solution {
-
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = sumOfSquareDigits(n);
-        while (fast != 1 && fast != slow) {
-            slow = sumOfSquareDigits(slow);
-            fast = sumOfSquareDigits(sumOfSquareDigits(fast));
+        
+        Set<Integer> set = new HashSet<>();
+        while(!set.contains(n)) {
+            set.add(n);
+            long sum = 0;
+            while(n != 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+            n = (int)sum;
+            if(n == 1) {
+                return true;
+            }
+            
         }
-        return fast == 1;
-    }
-
-    private int sumOfSquareDigits(int n) {
-        int sum = 0;
-        while (n != 0) {
-            int digit = n % 10;
-            sum += Math.pow(digit, 2);
-            n = n / 10;
-        }
-        return sum;
+        return false;
     }
 }
