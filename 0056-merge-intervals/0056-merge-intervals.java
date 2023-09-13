e0121b17a17da6f81fc8cf7852d1aa31c33ccdd2
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-09-13 17:01:35 -0500

    Time: 37 ms (5.15%), Space: 45.3 MB (71.93%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,33 +1,28 @@
 class Solution {
+
     public int[][] merge(int[][] intervals) {
-        // Sort the intervals by their start time
-        Arrays.sort(intervals, (a, b) -> a[0] -  b[0]);
-        
-        // Create a list to store the merged intervals
-        List<int[]> mergedIntervals = new ArrayList<>();
-        
-        // Iterate through the intervals and merge overlapping one
-        int i = 0;
-        while(i < intervals.length) {
+        List<int[]> result = new ArrayList<>();
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        result.add(intervals[0]);
+        int index = 0;
+
+        for (int i = 1; i < intervals.length; i++) {
+            int end = result.get(index)[1];
             int start = intervals[i][0];
-            int end = intervals[i][1];
-            
-            // FInd the end of the merged interval
-            while(i < intervals.length - 1 && intervals[i + 1][0] <= end) {
-                i++;
-                end = Math.max(end, intervals[i][1]);
+            if (start <= end) {
+                int[] interval = result.remove(index);
+                index--;
+                System.out.println("[" + interval[0] + ", " + interval[1] + "]");
+                interval[1] = Math.max(interval[1],intervals[i][1]);
+                result.add(interval);
+                index++;
+            } else {
+                result.add(intervals[i]);
+                index++;
             }
-            
-            // Add merged interval to the list
-            mergedIntervals.add(new int[]{start, end});
-            i++;
         }
-        
-        // Convert the list of merget intervlas to an array
-        int[][] result = new int[mergedIntervals.size()][2];
-        for(i = 0; i < result.length; i++) {
-            result[i] = mergedIntervals.get(i);
-        }
-        return result;
+
+        return result.toArray(new int[result.size()][]);
     }
 }
