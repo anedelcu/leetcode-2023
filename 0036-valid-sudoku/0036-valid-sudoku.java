5ce33ada8aff89bd783bcbd9cfd0e61057bf7381
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-09-20 11:12:57 -0500

    Time: 8 ms (39.20%), Space: 44.3 MB (11.40%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,27 +1,65 @@
 class Solution {
+
     public boolean isValidSudoku(char[][] board) {
-        
-        boolean[][] rows = new boolean[9][9];
-        boolean[][] columns = new boolean[9][9];
-        boolean[][] subBoxes = new boolean[9][9];
-        
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
-                char digit = board[i][j];
-                if(digit != '.') {
-                    int num = digit - '1';
-                    int subBoxIndex = (i / 3) * 3 + j / 3;
-                    
-                    if(rows[i][num] || columns[j][num] || subBoxes[subBoxIndex][num]) {
-                        return false; // Duplicate digit found
-                    }
-                    
-                    rows[i][num] = true;
-                    columns[j][num] = true;
-                    subBoxes[subBoxIndex][num] = true;
+                if(!isValid(board, i, j)) {
+                    return false;
                 }
             }
         }
         return true;
+    }
+
+    private boolean isValidRow(char[][] board, int row, int col) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            char c = board[i][col];
+            if (set.contains(c)) {
+                return false;
+            }
+            if (c != '.') {
+                set.add(c);
+            }
+        }
+        return true;
+    }
+
+    private boolean isValidCol(char[][] board, int row, int col) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            char c = board[col][i];
+            if (set.contains(c)) {
+                return false;
+            }
+            if (c != '.') {
+                set.add(c);
+            }
+        }
+        return true;
+    }
+
+    private boolean isValidBox(char[][] board, int row, int col) {
+        Set<Character> set = new HashSet<>();
+        int localRow = row - row % 3;
+        int localCol = col - col % 3;
+        for (int i = localRow; i < localRow + 3; i++) {
+            for (int j = localCol; j < localCol + 3; j++) {
+                char c = board[i][j];
+                if (set.contains(c)) {
+                    return false;
+                }
+                if (c != '.') {
+                    set.add(c);
+                }
+            }
+        }
+        return true;
+    }
+    private boolean isValid(char[][] board, int row, int col) {
+        if(isValidBox(board, row, col) && isValidRow(board, row, col) && isValidCol(board, row, col)) {
+            return true;
+        }
+        return false;
     }
 }
