a82560a5e917a1bf4102a8dabcf23bbb6854d782
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2023-10-06 11:10:56 -0500

    Time: 5 ms (96.48%), Space: 41.5 MB (84.27%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,21 +1,19 @@
-class Solution {
-    public boolean checkInclusion( String pattern, String str) {
-        if (pattern == null || str == null || pattern.length() > str.length()) {
-            return false;
+public class Solution {
+
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int[] freq = new int[26];
+        int m = s2.length();
+        for (int i = 0; i < n; i++) {
+            freq[s1.charAt(i) - 'a']++;
         }
-        int[] count1 = new int[26];
-        int[] count2 = new int[26];
-        for (char c : pattern.toCharArray()) {
-            count1[c - 'a']++;
-        }
-        for (int i = 0; i < str.length(); i++) {
-            if (i >= pattern.length()) {
-                count2[str.charAt(i - pattern.length()) - 'a']--;
+        int[] freq2 = new int[26];
+        for (int i = 0; i < m; i++) {
+            freq2[s2.charAt(i) - 'a']++;
+            if (i >= n) {
+                freq2[s2.charAt(i - n) - 'a']--;
             }
-            count2[str.charAt(i) - 'a']++;
-            if (Arrays.equals(count1, count2)) {
-                return true;
-            }
+            if (Arrays.equals(freq, freq2)) return true;
         }
         return false;
     }
