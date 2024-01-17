c9e2aee81ddae44a8ba2f91299530eeb07959011
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2024-01-17 17:28:46 -0600

    Time: 17 ms (92.12%), Space: 57 MB (56.83%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.java
+++ 0853-car-fleet/0853-car-fleet.java
@@ -1,24 +1,22 @@
 class Solution {
 
     public int carFleet(int target, int[] position, int[] speed) {
-        if (position.length == 1) {
-            return 1;
+         int res = 0;
+       double[] timeArr = new double[target];
+       for (int i = 0; i < position.length; i++)
+        {
+            timeArr[position[i]]= (double)(target - position[i]) / speed[i];
         }
-        int[][] combine = new int[position.length][2];
-        for (int i = 0; i < position.length; i++) {
-            combine[i][0] = position[i];
-            combine[i][1] = speed[i];
-        }
-        Arrays.sort(combine, java.util.Comparator.comparingInt(o -> o[0]));
-        Stack<Double> stack = new Stack<>();
-        for (int i = combine.length - 1; i >= 0; i--) {
-            double currentTime = (double) (target - combine[i][0]) / combine[i][1];
-            if (!stack.isEmpty() && currentTime <= stack.peek()) {
-                continue;
-            } else {
-                stack.push(currentTime);
+        double prev = 0.0;
+        for (int i = target-1; i >=0 ; i--)
+        {
+            double cur = timeArr[i];
+            if (cur > prev)
+            {
+                prev = cur;
+                res++;
             }
         }
-        return stack.size();
+        return res;  
     }
 }
