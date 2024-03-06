be37ab65d13c248eb70bac90eeba7c989f6786ee
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2024-03-05 18:13:43 -0600

    Time: 0 ms (100.00%), Space: 41.6 MB (67.32%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,14 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
         int res = 0;
-        for(int i = s.length() -1; i>=0; i--) {
-            if(s.charAt(i) != ' ') {
-                res++;
-            }
-            else {
-                break;
-            }
+        s = s.trim();
+        int i = s.length() -1;
+        while(i >=0 && s.charAt(i) != ' ') {
+            res++;
+            i--;
         }
         return res;
     }
