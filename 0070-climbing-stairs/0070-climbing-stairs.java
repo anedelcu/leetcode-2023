ee8b2b8ef9a2fe143bf6ff604efb44edc6832516
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2024-03-05 20:34:15 -0600

    Time: 0 ms (100.00%), Space: 40.1 MB (70.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
+        int one  = 1;
+        int two = 1;
         if(n == 1) {
             return 1;
         }
-        dp[n - 1] = 1;
-        dp[n] = 1;
-        for(int i  = n - 2; i >= 0; i--) {
-            dp[i] = dp[i + 1] + dp[i + 2];
+        for(int i = 0; i < n -1; i++) {
+            int temp = one;
+            one = one + two;
+            two = temp;
         }
         
-        return dp[0];
+        return one;
     }
 }
