32976ebca0709728bd93d2b8ad71f371606bc897
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2024-03-11 16:15:14 -0500

    Time: 2 ms (98.12%), Space: 43.5 MB (62.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,16 @@
 class Solution {
 
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        int[] arr = new int[26];
+        int[] chars = new int[26];
+
         for (char c : s.toCharArray()) {
-            arr[c - 'a']++;
+            chars[c - 'a']++;
         }
         for (char c : t.toCharArray()) {
-            arr[c - 'a']--;
+            chars[c - 'a']--;
         }
-        for (int i : arr) {
-            if (i != 0) {
+        for (int n : chars) {
+            if (n != 0) {
                 return false;
             }
         }
