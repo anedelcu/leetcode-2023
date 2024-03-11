056f9dbb8823458c155878f31114faad2955a84a
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2024-03-11 16:46:27 -0500

    Time: 6 ms (96.38%), Space: 47.9 MB (36.24%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,35 @@
 class Solution {
 
     public List<List<String>> groupAnagrams(String[] strs) {
+        List<ArrayList<String>> res = new ArrayList<>();
         Map<String, ArrayList<String>> map = new HashMap<>();
-        for(String s : strs) {
-            char[] chars = new char[26];
-            for (char c : s.toCharArray()) {
-                chars[c - 'a']++;
-            }
-            String keyStr = String.valueOf(chars);
-            ArrayList<String> anagrams = map.getOrDefault(keyStr, new ArrayList<String>());
-            anagrams.add(s);
-            map.put(keyStr, anagrams);
+        for(int i = 0; i < strs.length; i++) {
+            char[] s = strs[i].toCharArray();
+            Arrays.sort(s);
+            String key = String.valueOf(s);
+            ArrayList<String> anagrams = map.getOrDefault(key,  new ArrayList<String>());
+            anagrams.add(strs[i]);
+            map.put(key, anagrams);
         }
         return new ArrayList<>(map.values());
+    }
+    
+    private boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) {
+            return false;
+        }
+        int[] chars = new int[26];
+        for(char c : s.toCharArray()) {
+            chars[c - 'a']++;
+        }
+        for(char c : t.toCharArray()) {
+            chars[c - 'a']--;
+        }
+        for(int n : chars) {
+            if(n != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
