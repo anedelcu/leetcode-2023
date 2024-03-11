de111ec842e3d2d14d93aa0c2217f4426cb577e0
Alin Nedelcu <77385957+anedelcu@users.noreply.github.com>
2024-03-11 16:21:17 -0500

    Time: 2 ms (97.96%), Space: 44.9 MB (31.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,13 +5,14 @@ class Solution {
         // iterate through the array of nums
         //  check is the map contains the key of (target - num)
         // if yes we have an answer - else we add the num to the map
+        
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                return new int[] { i, map.get(target - nums[i]) };
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
             }
             map.put(nums[i], i);
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
